a3c8db74cfd77a88389ceca697432be8e6f44328
aayushiJoshi <aayushijoshi9910@gmail.com>
2025-03-08 20:26:38 +0530

    added getAllUrls variable in all static router endpoints

--- controller/static.js
+++ controller/static.js
@@ -13,20 +13,26 @@ async function handleHomePage(req,res){
 }
 
 async function handleURLgenerated(req, res){
+    const getAllUrls = await urlModel.find({});
+
     const encodedData = req.query.data;
     const ob = JSON.parse(decodeURIComponent(encodedData));
     // earlier I was rendering whole new ejs file [info.ejs], now making changes to home.ejs only
     return res.render("home",{
+        urls : getAllUrls,
         shortURL : ob.shortId,
         analyticsLink : `http://localhost:${process.env.PORT}/api/urlShortner/analytics/${ob.shortParam}`
     });
 }
 
 async function handleAnalytics(req,res){
+    const getAllUrls = await urlModel.find({});
+
     const encodedData = req.query.data;
     const ob = JSON.parse(decodeURIComponent(encodedData));
     // earlier I was rendering whole new ejs file [analytics.ejs], now making changes to home.ejs only
     return res.render("home",{
+        urls : getAllUrls,
         noOfVisits : ob.noOfVisits,
         analytics : ob.analytics
     })
